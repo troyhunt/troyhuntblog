624963a3b3ec537ffcda8d334eed62699e039692
Troy Hunt <troyhunt@hotmail.com>
2012-11-07 06:19:38 +1100

    Added SVG to PNG fallback with modernizr

---
+++ Scripts/TroyHunt.js
@@ -0,0 +1,8 @@
+﻿$(document).ready(function () {
+  if (!Modernizr.svg) {
+    var images = $('img[data-png-fallback]');
+    images.each(function (i) {
+      $(this).attr('src', $(this).data('png-fallback'));
+    });
+  }
+});
