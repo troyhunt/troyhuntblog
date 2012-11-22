5cd5779281549e6e5f47aa3f0f1303942b8dc7d6
Troy Hunt <troyhunt@hotmail.com>
2012-11-22 23:53:54 +1100

    Added Google Analytics into the JS

--- Web/Scripts/TroyHunt.js
+++ Web/Scripts/TroyHunt.js
@@ -1,4 +1,5 @@
-﻿$(document).ready(function () {
+﻿// Replace SVG with PNG if not supported
+$(document).ready(function () {
   if (!Modernizr.svg) {
     var images = $('img[src$=".svg"]');
     images.each(function (i) {
@@ -7,11 +8,23 @@
   }
 });
 
+// Don't put links underimages
 $('img').parent('a').addClass('imageLink');
 
+// Disqus
 var disqus_shortname = 'troyhunt';
 (function () {
   var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
   dsq.src = 'http://' + disqus_shortname + '.disqus.com/embed.js';
   (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+})();
+
+// Google Analytics
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-10949399-1']);
+_gaq.push(['_trackPageview']);
+(function () {
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'stats.g.doubleclick.net/dc.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
 })();
