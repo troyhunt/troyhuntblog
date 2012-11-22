067b43b1006ce2860d396ce38e3a4e340daa1d23
Troy Hunt <troyhunt@hotmail.com>
2012-11-22 23:43:08 +1100

    Optimised JS load sequence and arrow SVG

--- Web/Scripts/TroyHunt.js
+++ Web/Scripts/TroyHunt.js
@@ -8,3 +8,10 @@
 });
 
 $('img').parent('a').addClass('imageLink');
+
+var disqus_shortname = 'troyhunt';
+(function () {
+  var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+  dsq.src = 'http://' + disqus_shortname + '.disqus.com/embed.js';
+  (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+})();
