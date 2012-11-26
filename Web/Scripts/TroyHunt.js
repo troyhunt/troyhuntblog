b12147b4fdbdc4baf4889a675bf31891701be503
Troy Hunt <troyhunt@hotmail.com>
2012-11-26 22:11:18 +1100

    Last commit before serious templating!

--- Web/Scripts/TroyHunt.js
+++ Web/Scripts/TroyHunt.js
@@ -1,23 +1,25 @@
 ﻿// Replace SVG with PNG if not supported
-$(document).ready(function () {
-  if (!Modernizr.svg) {
-    var images = $('img[src$=".svg"]');
-    images.each(function (i) {
-      $(this).attr('src', $(this).attr('src').replace('.svg', '.png'));
-    });
-  }
-});
+if (!Modernizr.svg) {
+  var images = $('img[src$=".svg"]');
+  images.each(function (i) {
+    $(this).attr('src', $(this).attr('src').replace('.svg', '.png'));
+  });
+}
 
 // Don't put links under images
 $('img').parent('a').addClass('imageLink');
 
-// Disqus
-var disqus_shortname = 'troyhunt';
-(function () {
-  var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
-  dsq.src = 'http://' + disqus_shortname + '.disqus.com/embed.js';
-  (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
-})();
+// Disqus (breaks for old browsers - sorry ludites, no comments for you!)
+if (!$.browser.msie || parseInt($.browser.version, 10) > 8) {
+  var disqus_shortname = 'troyhunt';
+  (function () {
+    var dsq = document.createElement('script');
+    dsq.type = 'text/javascript';
+    dsq.async = true;
+    dsq.src = 'http://' + disqus_shortname + '.disqus.com/embed.js';
+    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+  })();
+}
 
 // Google Analytics
 var _gaq = _gaq || [];
