4fb1c260ff545453151ffc44a572a64327ed01ed
Troy Hunt <troyhunt@hotmail.com>
2012-12-02 08:04:59 +1100

    Used web optimizaition framework to bundle and minify

---
+++ Web/App_Start/BundleConfig.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Optimization;
+
+namespace Web.App_Start
+{
+  public class BundleConfig
+  {
+    public static void RegisterBundles(BundleCollection bundles)
+    {
+      bundles.Add(new ScriptBundle("~/Scripts/html5shiv").Include(
+                  "~/Scripts/html5shiv.js"));
+
+      bundles.Add(new ScriptBundle("~/Scripts/TroyHunt.min.js").Include(
+                  "~/Scripts/modernizr-*",
+                  "~/Scripts/TroyHunt.js"));
+
+      bundles.Add(new StyleBundle("~/TroyHunt.min.css").Include("~/TroyHunt.css"));
+    }
+  }
+}
---
+++ Web/Global.asax.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Web;
+using System.Web.Optimization;
+using Web.App_Start;
+
+namespace Web
+{
+  public class Global : HttpApplication
+  {
+
+    protected void Application_Start(object sender, EventArgs e)
+    {
+      BundleConfig.RegisterBundles(BundleTable.Bundles);
+    }
+  }
+}
