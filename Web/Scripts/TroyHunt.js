da4b2033a9f367628a6348e1fc5fb6f70d06adc5
Troy Hunt <troyhunt@hotmail.com>
2012-12-06 22:14:03 +1100

    Comment count done

--- Web/Scripts/TroyHunt.js
+++ Web/Scripts/TroyHunt.js
@@ -10,8 +10,8 @@ if (!Modernizr.svg) {
 $('img').parent('a').addClass('imageLink');
 
 // Disqus (breaks for old browsers - sorry ludites, no comments for you!)
+var disqus_shortname = 'troyhunt';
 if (!$.browser.msie || parseInt($.browser.version, 10) > 8) {
-  var disqus_shortname = 'troyhunt';
   (function () {
     var dsq = document.createElement('script');
     dsq.type = 'text/javascript';
@@ -20,6 +20,14 @@ if (!$.browser.msie || parseInt($.browser.version, 10) > 8) {
     (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
   })();
 }
+
+// Disqus comment count
+(function () {
+  var s = document.createElement('script'); s.async = true;
+  s.type = 'text/javascript';
+  s.src = 'http://' + disqus_shortname + '.disqus.com/count.js';
+  (document.getElementsByTagName('HEAD')[0] || document.getElementsByTagName('BODY')[0]).appendChild(s);
+}());
 
 // Google Analytics
 var _gaq = _gaq || [];
