337c0c569f22d4def1c5a8fe3b1519d837de04d2
Troy Hunt <troyhunt@hotmail.com>
2013-12-17 16:05:49 +1100

    Updated Disqus script per current recommendation.

--- Web/Scripts/TroyHunt.js
+++ Web/Scripts/TroyHunt.js
@@ -12,12 +12,9 @@ $('img').parent('a').addClass('imageLink');
 // Disqus (breaks for old browsers - sorry ludites, no comments for you!)
 var disqus_shortname = 'troyhunt';
 if (!$.browser.msie || parseInt($.browser.version, 10) > 8) {
-  var disqus_url = [location.protocol, '//', location.host, location.pathname].join('');
   (function () {
-    var dsq = document.createElement('script');
-    dsq.type = 'text/javascript';
-    dsq.async = true;
-    dsq.src = 'http://' + disqus_shortname + '.disqus.com/embed.js';
+    var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+    dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
     (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
   })();
 }
@@ -26,7 +23,7 @@ if (!$.browser.msie || parseInt($.browser.version, 10) > 8) {
 (function () {
   var s = document.createElement('script'); s.async = true;
   s.type = 'text/javascript';
-  s.src = 'http://' + disqus_shortname + '.disqus.com/count.js';
+  s.src = '//' + disqus_shortname + '.disqus.com/count.js';
   (document.getElementsByTagName('HEAD')[0] || document.getElementsByTagName('BODY')[0]).appendChild(s);
 }());
 
